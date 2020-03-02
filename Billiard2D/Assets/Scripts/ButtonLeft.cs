770d959990c0d7a198cd8398071d77237c34e43e
puramu818 <aa7368926@gmail.com>
2020-03-02 17:04:06 +0900

    ステージセレクト追加

---
+++ Billiard2D/Assets/Scripts/ButtonLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLeft : MonoBehaviour
+{
+    public bool buttonLeft = false;
+
+    public void OnClick()
+    {
+        buttonLeft = true;
+    }
+}
---
+++ Billiard2D/Assets/Scripts/ButtonRight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRight : MonoBehaviour
+{
+    public bool buttonRight = false;
+
+    public void OnClick()
+    {
+        buttonRight = true;
+    }
+}
