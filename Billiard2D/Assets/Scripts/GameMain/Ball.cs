ee8e512f14017881d3d3a17d7383c8a1d7443a97
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-03-03 13:23:47 +0900

    GameMainのテストをいじったぞぃ

---
+++ Billiard2D/Assets/Scripts/GameMain/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallType
+{
+    Blue,
+    Red
+};
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    BallType initType;
+    public BallType type { get; private set; }
+
+    Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期タイプを代入
+        type = initType;
+        // コンポーネントの取得
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 新しい状態を外部からセットし、新しいBodyを生成する
+    public void ChangeState(BallType _type)
+    {
+        this.type = _type;
+        CreateNewBody();
+    }
+
+    [SerializeField]
+    GameObject BluePref = null;
+    [SerializeField]
+    GameObject RedPref = null;
+    // 現在の状態を見て、新しいBodyを生成する
+    void CreateNewBody()
+    {
+        // 現在のBodyを検索し、破壊する
+        foreach (Transform c in this.transform)
+        {
+            if (c.gameObject.tag == "Body")
+                GameObject.Destroy(c.gameObject);
+        }
+
+        // 新しいBodyを生成
+        GameObject body = null;
+        switch (type)
+        {
+            case BallType.Blue:body = BluePref; break;
+            case BallType.Red: body = RedPref; break;
+        }
+        Instantiate(body, this.transform);
+    }
+}
---
+++ Billiard2D/Assets/Scripts/GameMain/StageManagement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManagement : MonoBehaviour
+{
+    int stageNum;
+    [SerializeField]
+    GameObject[] stages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ステージ番号を取得
+        SetCurrentStageNum();
+        // ステージを生成
+        CreateStage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 保存されている番号を取得。取得に失敗すると、0番がセットされる
+    void SetCurrentStageNum()
+    {
+        if (PlayerPrefs.HasKey("StageNum"))
+        {
+            stageNum = PlayerPrefs.GetInt("StageNum");
+        }
+        else
+        {
+            stageNum = 0;
+        }
+    }
+
+    // ステージ番号を見て、ステージを生成する
+    void CreateStage()
+    {
+        GameObject obj = Instantiate(stages[stageNum], this.transform.position, Quaternion.identity);
+    }
+}
